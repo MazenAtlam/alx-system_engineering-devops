95022d2cb673b87a8f8e8e7de4e20bfda400192c
MazenAtlam <mazenatef5510@gmail.com>
2024-07-13 15:36:45 -0700

    fix mode

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,7 @@
 # create a file
 file { '/tmp/school':
-    mode    => '0774',
+    path    => '/tmp/school',
+    mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
     content => 'I love Puppet',
